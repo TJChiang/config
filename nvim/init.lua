41f958a279f8b0838074af2d258893f08e34c52b
TJChiang <tjchiang8419@gmail.com>
2026-03-07 15:52:53 +0800

    nvim: add function to close current buffer and update keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -381,14 +381,29 @@ require("lazy").setup({
 })
 
 -- Keymaps
+local function close_current_buffer_keep_nvim()
+  local current = vim.api.nvim_get_current_buf()
+  local listed = vim.fn.getbufinfo({ buflisted = 1 })
+
+  if #listed > 1 then
+    vim.cmd("bprevious")
+    vim.cmd("bdelete " .. current)
+  else
+    vim.cmd("enew")
+    vim.cmd("bdelete " .. current)
+  end
+end
+
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Toggle explorer" })
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>t", "<cmd>Telescope oldfiles<cr>", { desc = "Recent files" })
+
 vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
 vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Problems panel" })
 vim.keymap.set("n", "<leader>xw", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { desc = "Buffer problems" })
-vim.keymap.set("n", "<leader>w", "<cmd>bd<cr>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>w", close_current_buffer_keep_nvim, { desc = "Close buffer" })
 vim.keymap.set("n", "<leader>]", "<cmd>bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>[", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit window" })
